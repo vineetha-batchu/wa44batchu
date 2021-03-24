66f08e355d2987185b3d494305c420bff3d7d4c4
vineetha-batchu <77755310+vineetha-batchu@users.noreply.github.com>
2021-03-24 15:51:47 -0500

    part 5

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var i=0;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  i+=1;
+  res.send('User accesses are: '+i);
 });
 
 module.exports = router;
